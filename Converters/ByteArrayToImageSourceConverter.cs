8d0d8a754bbc599e7443e6c1513cc76aa7e8a978
Alesha Auckram <85319825+AleshaAuckram@users.noreply.github.com>
2023-11-22 12:07:23 +1300

    Add files via upload

---
+++ Converters/ByteArrayToImageSourceConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Graphics;
+
+namespace RecipeApp.Converters
+{
+    [ContentProperty(nameof(Source))]
+    public class ByteArrayToImageSourceConverter : IValueConverter, IMarkupExtension
+    {
+        public ImageSource Source { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte[] byteArray)
+            {
+                return ImageSource.FromStream(() => new System.IO.MemoryStream(byteArray));
+            }
+
+            return Source;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ Converters/StrikeThroughConverter.cs
@@ -0,0 +1,27 @@
+﻿// StrikeThroughConverter.cs
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace RecipeApp.Converters
+{
+    public class StrikeThroughConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 1 && values[0] is bool isChecked)
+            {
+                if (isChecked)
+                {
+                    return TextDecorations.Strikethrough;
+                }
+            }
+            return TextDecorations.None;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
